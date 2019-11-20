6d2adc3f2cc266f4197429dcdd885ae98b107666
Paul Catalan <contact.pcatl@gmail.com>
2019-11-20 13:31:43 +0800

    Added snippet for dependency injection

---
+++ snippets/polymorphic_dependecy_injection_via_constructor_or_method.cpp
@@ -0,0 +1,44 @@
+#include "testinc.hpp"
+
+// Base light bulb
+class LightBulb
+{
+};
+
+
+// Child class
+class LightBulbBrandA : public LightBulb
+{
+};
+
+class LightBulbBrandB : public LightBulb
+{
+};
+
+class Lamp
+{
+public:
+    Lamp(LightBulb bulb) :lightBulb(bulb) {};
+
+    void setBulb(LightBulb bulb)
+    {
+        lightBulb = bulb;
+    }
+
+private:
+    LightBulb lightBulb;
+};
+
+int main()
+{
+
+    LightBulbBrandA bulbA;
+    LightBulbBrandB bulbB;
+
+    Lamp lamp(bulbA); // constructor injection
+    lamp.setBulb(bulbB); // method injection
+
+
+    return 0;
+}
+
